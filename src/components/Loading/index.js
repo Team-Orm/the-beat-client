3af28077c0491f4c8eb5baddee6c683d60226a6f
shuh319 <shuh319@gmail.com>
2023-03-12 15:47:00 +0900

    feat: 초기 설정

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,5 +1,19 @@
-import React from "react";
+/* eslint-disable prefer-template */
+import React, { useEffect, useState } from "react";
 
-export default function Loading() {
-  return <p>Loading</p>;
+export default function Loading({ text = "Loading", speed = 300 }) {
+  const [content, setContent] = useState(text);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // eslint-disable-next-line no-unused-expressions
+      content === text + "..."
+        ? setContent(text)
+        : setContent((prevContent) => prevContent + ".");
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [content, speed, text]);
+
+  return <p>{content}</p>;
 }
