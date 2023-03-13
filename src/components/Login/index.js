005301da0dda370242d8320872bb8f197c28c7e3
shuh319 <shuh319@gmail.com>
2023-03-13 18:35:27 +0900

    feat: 초기 설정_Final

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,15 +1,46 @@
 import React from "react";
-import "./index.css";
+import styled from "styled-components";
 
 export default function Login() {
   return (
-    <div className="login-container">
-      <h1>The Beat</h1>
-      <form>
-        <button type="submit" className="circular-button">
-          Login
-        </button>
-      </form>
-    </div>
+    <LoginContainer>
+      <Title>The Beat</Title>
+      <CircularButton type="submit">Login</CircularButton>
+    </LoginContainer>
   );
 }
+
+const LoginContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("login.png");
+  background-size: cover;
+  background-position: center;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 5em;
+  color: white;
+`;
+
+const CircularButton = styled.button`
+  width: 200px;
+  height: 75px;
+  background-color: transparent;
+  border-radius: 10px;
+  border: 3px solid rgba(255, 255, 255, 0.5);
+  font-size: 2em;
+  color: white;
+
+  :hover {
+    color: skyBlue;
+    border: 3px solid skyBlue;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store/configureStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
 );
