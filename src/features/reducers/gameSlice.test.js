5b7a1141c0f1390d4528adb11ee4a72e5eac8be7
oyobbeb <oyobbeb@gmail.com>
2023-03-29 00:04:56 +0900

    add: Unit Redux와 NotFound 테스트 추가

---
+++ src/features/reducers/gameSlice.test.js
@@ -0,0 +1,68 @@
+import gameReducer, {
+  updateScore,
+  updateCombo,
+  isSongEnd,
+  updateWord,
+} from "./gameSlice";
+
+describe("gameSlice", () => {
+  const initialState = {
+    score: 0,
+    totalScore: 0,
+    comboResults: {
+      excellent: 0,
+      good: 0,
+      miss: 0,
+    },
+    currentCombo: 0,
+    end: false,
+    word: "",
+  };
+
+  it("should handle initial state", () => {
+    expect(gameReducer(undefined, { type: "unknown" })).toEqual(initialState);
+  });
+
+  it("should update score", () => {
+    const score = 100;
+    const expectedState = { ...initialState, score };
+
+    expect(gameReducer(initialState, updateScore(score))).toEqual(
+      expectedState,
+    );
+  });
+
+  it("should update combo", () => {
+    const currentCombo = 2;
+    const expectedState = { ...initialState, currentCombo };
+
+    expect(gameReducer(initialState, updateCombo(currentCombo))).toEqual(
+      expectedState,
+    );
+  });
+
+  it("should set song end state", () => {
+    const payload = {
+      comboResults: { excellent: 3, good: 2 },
+      currentScore: 120,
+      maxNotesNumber: 10,
+    };
+    const expectedState = {
+      ...initialState,
+      totalScore: payload.currentScore,
+      comboResults: { excellent: 3, good: 2, miss: 5 },
+      end: true,
+    };
+
+    expect(gameReducer(initialState, isSongEnd(payload))).toEqual(
+      expectedState,
+    );
+  });
+
+  it("should update word", () => {
+    const word = "Example";
+    const expectedState = { ...initialState, word };
+
+    expect(gameReducer(initialState, updateWord(word))).toEqual(expectedState);
+  });
+});
